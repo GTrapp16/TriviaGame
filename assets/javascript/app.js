e499de75cfb3a0cf631795efae785146116d13ac
GTrapp16 <GTrapp16@gmail.com>
2018-03-05 21:18:35 -0700

    skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+var questionList= [{
+	question: "Who is James Bond's rival in Goldeneye 007?",
+	answerList: ["Oddjob", "Alec Trevelyan", "Jaws", "Boris Greshenko"],
+	answer: 1
+},{
+	question: "Who is the main protagonist in Zelda Ocarina of Time?",
+	answerList: ["Zelda", "Mario", "Navi", "Link"],
+	answer: 3
+},{
+	question: "Who is the main antagonist in Starfox?",
+	answerList: ["Sephiroth", "Frieza", "Andross", "Bowser"],
+	answer: 2
+},{
+	question: "In what game can you fly a hang glider, gyrocopter, and jetpack?",
+	answerList: ["Hangtime", "Spyro", "Flying Dragon", "Pilotwings 64"],
+	answer: 3
+},{
+	question: "What form does the final boss take in Super Smash Brothers 64?",
+	answerList: ["Hand", "Hat", "Face", "Spirit"],
+	answer: 0
+},{
+	question: "In what game does an evil witch attempt to steal the youth and beauty from your younger sister?",
+	answerList: ["Majora's Mask", "Banjo-Kazooie", "Conker's Bad Fur Day", "Quest"],
+	answer: 1
+},{
+	question: "What CIA agent works to stop a terrorist plot while working against an unknwown mole?",
+	answerList: ["006", "Ethan Hawk", "James Bond", "Ethan Hunt"],
+	answer: 2
+},{
+	question: "Who designed Zelda Ocarina of Time?",
+	answerList: ["Hideo Kojima", "Shigeru Miyamoto", "Sid Meier", "Will Wright"],
+	answer: 1
+},{
+	question: "Which is not a racing game on the N64?",
+	answerList: ["Driver 64", "Mario Kart 64", "Diddy Kong Racing", "Wave Race 64"],
+	answer: 0
+},{
+	question: "Which were the first two games released the Nintendo 64??",
+	answerList: ["Super Mario 64 & Pilotwings 64", "Super Mario 64 & Mario Kart 64", "Mario Kart 64 & Pilotwings 64", "Super Mario 64 & Wave Racer 64"],
+	answer: 0
+}];
+
+
+$("#readyBtn").on('click', function(){
+    $(this).hide();
+    newGame();
+});
+
+function newGame(){
+
+    
+}
